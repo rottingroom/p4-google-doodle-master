f06ae19c58e83425e286dbc9eef2995173d162ee
Anthony Raffini <traffini@gmail.com>
2016-06-05 14:24:36 -0700

    A start for my doodle

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,53 @@
 // Animate Star
-var $star = $('#star');
+var $vcd = $('#vcd');
+var $vcdlogo = $('#vcdlogo');
+var $vcdshadow = $('#vcdshadow');
+var $V = $('.V');
+var $C = $('.C');
+var $D = $('.D');
+var $Vshadow = $('.Vshadow');
+var $Cshadow = $('.Cshadow');
+var $Dshadow = $('.Dshadow');
+var $stars = $('.stars')
 
-// DOM Element, Duration, CSS Properties 
-TweenMax.from($star, 1, {
+TweenMax.from($V, 1, {
     opacity: 0,
-    x: 180
+    scale: 0.1
 });
+
+TweenMax.from($Vshadow, 1, {
+    opacity: 0,
+    delay: .5,
+});
+
+TweenMax.from($C, 1, {
+    opacity: 0,
+    delay: 1,
+    scale: 0.1
+});
+
+TweenMax.from($Cshadow, 1, {
+    opacity: 0,
+    delay: 1.5,
+});
+
+TweenMax.from($D, 1, {
+    opacity: 0,
+    delay: 2,
+    scale: 0.1
+});
+
+TweenMax.from($Dshadow, 1, {
+    opacity: 0,
+    delay: 2.5,
+});
+
+TweenMax.staggerFrom($stars, 2, {
+    opacity: 0,
+    delay: 2
+}, 0.2);
+
+TweenMax.staggerTo($stars, 2, {
+    opacity: 0,
+    delay: 3
+}, 0.2);
